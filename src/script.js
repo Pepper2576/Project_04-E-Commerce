d57fdde04f9d078c47baf9bd0cbbf60628fa70b1
Chris Pepper <pepper2576@gmail.com>
2024-11-16 17:40:25 +0000

    decision to move away from using JSON due to ease for making a JS functionality.

--- src/script.js
+++ src/script.js
@@ -3,23 +3,27 @@ const displayEle = document.querySelector('.display');
 
 dateEle.innerHTML = new Date().getFullYear();
 
-async function loadData() {
-  try {
-    const res = await fetch('./data/items.json');
-    const data = await res.json();
-    console.log(data);
+// async function loadData() {
+//   try {
+//     const res = await fetch('./data/items.json');
+//     const data = await res.json();
+//     console.log(data);
 
-    displayEle.innerHTML = data
-      .map((i) => {
-        return `<div class="test">
-          <h3>${i.productName}</h3>
-          <img src="./data/${i.img}" alt="">
-          <p>£${i.price}</p>
-        </div>`;
-      })
-      .join('');
-  } catch (err) {
-    console.error(`Error loading JSON Data: ${err}`);
-  }
+//     displayEle.innerHTML = data
+//       .map((i) => {
+//         return `<div class="product-card">
+//           <h3>${i.productName}</h3>
+//           <p>£${i.price}</p>
+//           <img src="./data/${i.img}" alt="">
+//         </div>`;
+//       })
+//       .join('');
+//   } catch (err) {
+//     console.error(`Error loading JSON Data: ${err}`);
+//   }
+// }
+// loadData();
+
+function addToCart(name, cost, imgUrl) {
+  console.log(`${name}\n${cost}\n${imgUrl}`);
 }
-loadData();
