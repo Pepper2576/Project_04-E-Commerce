df752483ee12b088ad6bec182bc8ac14799bf493
Chris Pepper <pepper2576@gmail.com>
2024-11-18 13:32:33 +0000

    Basket model begun, still needs styling. middleware logic begun able to crate an array of products and track indervicual qtys

--- src/script.js
+++ src/script.js
@@ -1,5 +1,11 @@
 const dateEle = document.querySelector('#date');
 const displayEle = document.querySelector('.display');
+const hiddenEle = document.querySelector('.hidden-ele');
+const basketBtn = document.querySelector('#basket-icon-btn');
+
+let id1 = 0;
+let id2 = 0;
+let id3 = 0;
 
 dateEle.innerHTML = new Date().getFullYear();
 
@@ -23,7 +29,43 @@ dateEle.innerHTML = new Date().getFullYear();
 //   }
 // }
 // loadData();
+let cartArray = [];
+function addToCart(id, name, cost, imgUrl) {
+  console.log(`${id}\n${name}\n${cost}\n${imgUrl}`);
+  let productObject = {
+    productId: id,
+    productName: name,
+    productCost: cost,
+    productImg: imgUrl,
+    qty: 1,
+  };
+  cartArray.push(productObject);
+}
 
-function addToCart(name, cost, imgUrl) {
-  console.log(`${name}\n${cost}\n${imgUrl}`);
+basketBtn.addEventListener('click', () => {
+  if (hiddenEle.style.display != 'flex') {
+    checkCart();
+    hiddenEle.style.display = 'flex';
+  } else {
+    hiddenEle.style.display = 'none';
+  }
+});
+
+function checkCart() {
+  const idArray = [1, 2, 3];
+
+  for (let i = 0; i < cartArray.length; i++) {
+    if (cartArray[i].productId === idArray[0]) {
+      id1 += 1;
+    } else if (cartArray[i].productId === idArray[1]) {
+      id2 += 1;
+    } else if (cartArray[i].productId === idArray[2]) {
+      id3 += 1;
+    } else {
+      console.log(`${cartArray[i]} unknown product`);
+    }
+  }
+  console.log(id1);
+  console.log(id2);
+  console.log(id3);
 }
